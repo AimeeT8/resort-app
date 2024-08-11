f34750aba47736daa7cfd75eec8486b6ee3f9cb2
aimee_t <aimeetemple14@gmail.com>
2024-08-11 16:17:53 -0400

    Added resortView that will show more details for each resort

---
+++ SnowSeeker/ResortView.swift
@@ -0,0 +1,18 @@
+//
+//  ResortView.swift
+//  SnowSeeker
+//
+//  Created by Aimee Temple on 2024-08-11.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResortView()
+}
