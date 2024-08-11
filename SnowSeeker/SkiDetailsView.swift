bb308375da427c04524b4867e29e00d69617e669
aimee_t <aimeetemple14@gmail.com>
2024-08-11 16:26:23 -0400

    Added skiDetailsView.

---
+++ SnowSeeker/SkiDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  SkiDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Aimee Temple on 2024-08-11.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkiDetailsView()
+}
