298151422f04b9e6b8a3ca3e6c1f6481fe5e3b23
aimee_t <aimeetemple14@gmail.com>
2024-08-11 16:32:18 -0400

    Added ResortDetailsView.

---
+++ SnowSeeker/ResortDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  ResortDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Aimee Temple on 2024-08-11.
+//
+
+import SwiftUI
+
+struct ResortDetailsView: View {
+    let resort: Resort
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResortDetailsView()
+}
